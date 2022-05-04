2296aa339697d5e51c46c364401d9235382d2bb3
LeeJaeHoon <80977178+dkekdmf@users.noreply.github.com>
2022-05-04 09:28:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+ include_package_data = True,
+  name=mycalcul0001,
+  version'1.0.0',
+  description'oss-dev mycalcul',
+  author'dkekdmf',
+  author_email= 'dkekdmf@naver.com',
+  url = 'https://github.com/dkekdmf/mycalcul/',
+  install_requires= ['pytest'],
+  long_description= 'oss-dev mycalcul',
+  long_description_content_type='text/markdown',
+  classifiers=[
+   "Programming Language :: Python ::3",
+    "Operating System :: OS Independent",
+  ],
+  
+)
